d8a597be2be9f927734daf83cffb99d3a7514bf3
doardo9x <eduardo010304@gmail.com>
2025-02-19 10:49:06 -0300

    Start Projeto

---
+++ StickerSmash/app/index.tsx
@@ -0,0 +1,22 @@
+import { Text, View, StyleSheet } from "react-native";
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Home</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#25292e',
+    justifycontent:"center",
+    alignItems: "center",
+  },
+  text: {
+    color: '#fff',
+    
+  }
+})
