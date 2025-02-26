89ea858aa5e0a7ac67b092cc02a0b17e56b1263c
doardo9x <eduardo010304@gmail.com>
2025-02-26 10:51:41 -0300

    Incluindo navegação por abas

--- StickerSmash/app/(tabs)/index.tsx
+++ StickerSmash/app/(tabs)/index.tsx
@@ -1,9 +1,13 @@
+import { Link } from "expo-router";
 import { Text, View, StyleSheet } from "react-native";
 
 export default function Index() {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Home</Text>
+      <Link href="/about" style={styles.button}>
+        Sobre
+      </Link>
     </View>
   );
 }
@@ -12,11 +16,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#25292e',
-    justifycontent:"center",
+    justifyContent:"center",
     alignItems: "center",
   },
   text: {
     color: '#fff',
     
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
+    color:'#fff'
   }
 })
