31147ac206229311cc1d05627303682d4c165294
doardo9x <eduardo010304@gmail.com>
2025-04-02 10:39:00 -0300

    Criando o componente EmojiList

---
+++ StickerSmash/components/EmojiList.tsx
@@ -0,0 +1,18 @@
+import { UseState } from 'react'
+import { StyleSheet, FlatList, Platform, Pressable } from 'react-native'
+import { Image, type ImageSource } from 'expo-image'
+
+type Props = {
+    onSelect: (image: ImageSource) => void;
+    onCloseModal: () => void;
+}
+
+export default function EmojiList() {
+  return (
+    <View>
+      <Text>EmojiList</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
